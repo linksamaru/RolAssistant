6516f127184c5881664a1445a81a9f31dec7c40c
Linksamaru <snyper_39@hotmail.com>
2013-10-15 17:35:50 +0200

    Ficha 2.0 [15/10/2013]
    Ficha 2.0 en SWING y con una interfaz mas amistosa

--- Ficha2.java
+++ Ficha2.java
@@ -18,50 +18,64 @@ public class Ficha2 extends JFrame implements WindowListener{
     private JButton subSTR=new JButton("-");
     private JTextField textSTR=new JTextField();
     private JLabel labelSTR=new JLabel("Fuerza: ");
-    private JPanel panelSTR=new JPanel(new BorderLayout(4,1));
+    private JPanel panelSTR=new JPanel(new BorderLayout(1,4));
     
     //inteligencia
     private JButton addINT=new JButton("+");
     private JButton subINT=new JButton("-");
     private JTextField textINT=new JTextField();
     private JLabel labelINT=new JLabel("Inteligencia: ");
-    private JPanel panelINT=new JPanel(new BorderLayout(4,1));
+    private JPanel panelINT=new JPanel(new BorderLayout(1,4));
     
     //destreza
     private JButton addDEX=new JButton("+");
     private JButton subDEX=new JButton("-");
     private JTextField textDEX=new JTextField();
     private JLabel labelDEX=new JLabel("Destreza: ");
-    private JPanel panelDEX=new JPanel(new BorderLayout(4,1));
+    private JPanel panelDEX=new JPanel(new BorderLayout(1,4));
     
     //percepcion
     private JButton addPER=new JButton("+");
     private JButton subPER=new JButton("-");
     private JTextField textPER=new JTextField();
     private JLabel labelPER=new JLabel("Percepcion: ");
-    private JPanel panelPER=new JPanel(new BorderLayout(4,1));
+    private JPanel panelPER=new JPanel(new BorderLayout(1,4));
     
     //resistencia
     private JButton addRES=new JButton("+");
     private JButton subRES=new JButton("-");
     private JTextField textRES=new JTextField();
     private JLabel labelRES=new JLabel("Resistencia: ");
-    private JPanel panelRES=new JPanel(new BorderLayout(4,1));
+    private JPanel panelRES=new JPanel(new BorderLayout(1,4));
     
     //labels de apartados
     private JLabel Atrib=new JLabel("Atributos: ");
     private JLabel Habi=new JLabel("Habilidades: ");
-    private JLabel Dot=new JLabel("Dotes: ");
+    private JLabel Dote=new JLabel("Dotes: ");
     private JLabel Equi=new JLabel("Equipo: ");
     private JLabel Crear1=new JLabel("Created By: ");
     private JLabel Crear2=new JLabel("Linksamaru");
     
+    //botones de apartados
+    private JButton Hab=new JButton("Añadir Habilidades");
+    private JButton Dot=new JButton("Añadir Dotes");
+    private JButton Equ=new JButton("Añadir Equipo");
+    private JButton Fic=new JButton("Visualizar Ficha");
+    
     //paneles de apartados
-    private JPanel Atributos=new JPanel(new BorderLayout(1,2));
-    private JPanel Habilidades=new JPanel(new BorderLayout(1,2));
-    private JPanel Dotes=new JPanel(new BorderLayout(1,2));
-    private JPanel Equipo=new JPanel(new BorderLayout(1,2));
-    private JPanel Creador=new JPanel(new BorderLayout(1,2));
+    private JPanel Atributos=new JPanel(new BorderLayout(2,1));
+    private JPanel Visual=new JPanel();
+    private JPanel Habilidades=new JPanel(new BorderLayout(2,1));
+    private JPanel Dotes=new JPanel(new BorderLayout(2,1));
+    private JPanel Equipo=new JPanel(new BorderLayout(2,1));
+    private JPanel Creador=new JPanel(new BorderLayout(2,1));
+    
+    //paneles de ordenacion
+    private JPanel AtriVisua=new JPanel(new BorderLayout(2,1));
+    private JPanel AtriVisuaCrea=new JPanel(new BorderLayout(2,1));
+    private JPanel HabiDot=new JPanel(new BorderLayout(2,1));
+    private JPanel HabiDotEqu=new JPanel(new BorderLayout(2,1));
+    private JPanel General=new JPanel(new GridLayout(3,2));
     
     public Ficha2(){
         //panel atributo fuerza
@@ -79,21 +93,57 @@ public class Ficha2 extends JFrame implements WindowListener{
         this.panelDEX.add(addDEX);
         this.panelDEX.add(textDEX);
         this.panelDEX.add(subDEX);
-        //panel atributo fuerza
+        //panel atributo percepcion
         this.panelPER.add(labelPER);
         this.panelPER.add(addPER);
         this.panelPER.add(textPER);
         this.panelPER.add(subPER);
-        //panel atributo fuerza
+        //panel atributo resistencia
         this.panelRES.add(labelRES);
         this.panelRES.add(addRES);
         this.panelRES.add(textRES);
         this.panelRES.add(subRES);
         //panel atributos
-        this.Atributos.add(Atrib,BorderLayout.NORTH);
-        
-        
-        
+        this.Atributos.add(Atrib);
+        this.Atributos.add(panelSTR);
+        this.Atributos.add(panelINT);
+        this.Atributos.add(panelDEX);
+        this.Atributos.add(panelPER);
+        this.Atributos.add(panelRES);
+        //panel visualizacion
+        this.Visual.add(Fic);
+        //panel habilidades
+        this.Habilidades.add(Habi);
+        this.Habilidades.add(Hab);
+        //panel dotes
+        this.Dotes.add(Dote);
+        this.Dotes.add(Dot);
+        //panel equipo
+        this.Equipo.add(Equi);
+        this.Equipo.add(Equ);
+        //panel creador
+        this.Creador.add(Crear1);
+        this.Creador.add(Crear2);
+        /*paneles de ordenacion
+        * this.AtriVisua.add(Atributos);
+        * this.AtriVisua.add(Visual);
+        * this.AtriVisuaCrea(AtriVisua);
+        * this.AtriVisuaCrea(Creador);
+        * dado q no quiere funcionar lo añadire
+        * con una grid de 3x2 y rezare 
+        * al monstruo del espaguetti volador 
+        * para q funcione
+        */
+        this.General.add(Atributos);
+        this.General.add(Visual);
+        this.General.add(Creador);
+        this.General.add(Habilidades);
+        this.General.add(Dotes);
+        this.General.add(Equipo);
+        this.add(General);
+        this.setBounds(200, 200, 500, 600);
+        this.setVisible(true);
+        this.setResizable(true);
     }
     
     //funciones varias de ventana
@@ -106,4 +156,5 @@ public class Ficha2 extends JFrame implements WindowListener{
     public void windowDeiconified(WindowEvent e) {}
     public void windowActivated(WindowEvent e) {}
     public void windowDeactivated(WindowEvent e) {}
+
 }
